6c780f79760966e9dabd858cfc2b7358cff8bbce
YoungCapta1n <huzhuhuan1991@gmail.com>
2019-05-08 21:29:18 +0800

    sqlite

--- fileIO/include/database.h
+++ fileIO/include/database.h
@@ -16,10 +16,12 @@
 #include <string>
 #include <vector>
 #include "controllerdata.h"
+#include "easylogging++.h"
 #include "estimatordata.h"
 #include "gpsdata.h"
 #include "plannerdata.h"
 
+template <int m, int n = 3>
 class database {
  public:
   explicit database(const std::string &_savepath)
@@ -28,18 +30,76 @@ class database {
 
   ~database() {}
 
+  void initializetables() {
+    create_mastertable();
+    create_GPS_table();
+    create_controller_table();
+    create_estimator_table();
+    create_planner_table();
+  }
+  // insert a bow into gps table
   void update_gps_table(const gpsRTdata &_gpsRTdata) {
     try {
       std::string str =
           "INSERT INTO GPS"
           "(DATETIME, gps_week, gps_time, heading, pitch, roll, latitude,"
           "longitude, altitude, Ve, Vn, Vu, base_line, NSV1, NSV2,"
-          "status, check, UTM_x, UTM_y) VALUES( 0 , julianday('now')";
+          "gpsstatus, gpscheck, UTM_x, UTM_y) VALUES(julianday('now')";
       convert2string(_gpsRTdata, str);
+      str += ");";
+
+      db << str;
+    } catch (sqlite::sqlite_exception e) {
+      CLOG(ERROR, "sql") << e.what();
+    }
+  }
+  // insert a bow into controller table
+  void update_controller_table(const controllerRTdata<m, n> &_RTdata) {
+    try {
+      std::string str =
+          "INSERT INTO controller"
+          "(DATETIME ";
+
+      // tau: desired force
+      for (int i = 0; i != n; ++i) {
+        str += ", tau" + std::to_string(i + 1);
+      }
+      // the angle of each propeller
+      for (int i = 0; i != m; ++i) {
+        str += ", alpha" + std::to_string(i + 1);
+      }
+      // the speed of each propeller
+      for (int i = 0; i != m; ++i) {
+        str += ", rpm" + std::to_string(i + 1);
+      }
+      // BalphaU: estimated force
+      for (int i = 0; i != n; ++i) {
+        str += ", est" + std::to_string(i + 1);
+      }
+
+      str += ") VALUES(julianday('now')";
+      convert2string(_RTdata, str);
+      str += ");";
+
+      db << str;
+    } catch (sqlite::sqlite_exception e) {
+      CLOG(ERROR, "sql") << e.what();
+    }
+  }
+  // insert a bow into estimator table
+  void update_estimator_table(const estimatorRTdata &_RTdata) {
+    try {
+      std::string str =
+          "INSERT INTO estimator"
+          "(DATETIME, meas_x, meas_y, meas_theta, meas_u, meas_v, meas_r,"
+          "state_x, state_y, state_theta, state_u, state_v, state_r, "
+          "perror_x, perror_y, perror_mz, verror_x, verror_y, verror_mz"
+          ") VALUES(julianday('now')";
+      convert2string(_RTdata, str);
       str += ");";
       db << str;
     } catch (sqlite::sqlite_exception e) {
-      cout << "Unexpected error " << e.what() << endl;
+      CLOG(ERROR, "sql") << e.what();
     }
   }
 
@@ -54,108 +114,126 @@ class database {
 
   // create master table about the info of each tables
   void create_mastertable() {
-    db << "CREATE TABLE IF NOT EXISTS MASTER("
-          "CLIENT_ID    INT    PRIMARY KEY    NOT NULL,"
-          "TABLE_NAME   TEXT                          ,"
-          "DATETIME     TEXT                         );";
+    try {
+      db << "CREATE TABLE IF NOT EXISTS MASTER("
+            "CLIENT_ID    INT    PRIMARY KEY    NOT NULL,"
+            "TABLE_NAME   TEXT                          ,"
+            "DATETIME     TEXT                         );";
 
-    for (int i = 0; i != clientset.size(); ++i) {
-      // update clientset
-      std::string s_id = std::to_string(i);
-      // create string for sqlite
-      std::string str(
-          "INSERT INTO MASTER (CLIENT_ID,TABLE_NAME,DATETIME) VALUES(");
-      str += s_id;
-      str += ", ";
-      str += clientset[i];
-      str += "', DATETIME('now'));";
-      db << str;
+      for (unsigned int i = 0; i != clientset.size(); ++i) {
+        // update clientset
+        std::string s_id = std::to_string(i);
+        // create string for sqlite
+        std::string str(
+            "INSERT INTO MASTER (CLIENT_ID,TABLE_NAME,DATETIME) VALUES(");
+        str += s_id;
+        str += ", '";
+        str += clientset[i];
+        str += "' , DATETIME('now'));";
+        db << str;
+      }
+    } catch (sqlite::sqlite_exception e) {
+      CLOG(ERROR, "sql") << e.what();
     }
   }
 
   // create GPS table
   void create_GPS_table() {
-    // GPS/IMU sensor GPFPD format
-    std::string str =
-        "CREATE TABLE GPS"
-        "(ID          INTEGER PRIMARY KEY AUTOINCREMENT,"
-        " DATETIME    TEXT       NOT NULL,"
-        " gps_week    INT, "
-        " gps_time    DOUBLE, "
-        " heading     DOUBLE, "
-        " pitch       DOUBLE, "
-        " roll        DOUBLE, "
-        " latitude    DOUBLE, "
-        " longitude   DOUBLE, "
-        " altitude    DOUBLE, "
-        " Ve          DOUBLE, "
-        " Vn          DOUBLE, "
-        " Vu          DOUBLE, "
-        " base_line   DOUBLE, "
-        " NSV1        INT, "
-        " NSV2        INT, "
-        " status      TEXT, "
-        " check       TEXT, "
-        " UTM_x       DOUBLE, "
-        " UTM_y       DOUBLE);";
+    try {
+      // GPS/IMU sensor GPFPD format
+      std::string str =
+          "CREATE TABLE GPS"
+          "(ID          INTEGER PRIMARY KEY AUTOINCREMENT,"
+          " DATETIME    TEXT       NOT NULL,"
+          " gps_week    INT, "
+          " gps_time    DOUBLE, "
+          " heading     DOUBLE, "
+          " pitch       DOUBLE, "
+          " roll        DOUBLE, "
+          " latitude    DOUBLE, "
+          " longitude   DOUBLE, "
+          " altitude    DOUBLE, "
+          " Ve          DOUBLE, "
+          " Vn          DOUBLE, "
+          " Vu          DOUBLE, "
+          " base_line   DOUBLE, "
+          " NSV1        INT, "
+          " NSV2        INT, "
+          " gpsstatus   TEXT, "
+          " gpscheck    TEXT,"
+          " UTM_x       DOUBLE, "
+          " UTM_y       DOUBLE);";
+      db << str;
 
-    db << str;
+    } catch (sqlite::sqlite_exception e) {
+      CLOG(ERROR, "sql") << e.what();
+    }
   }
   // create each controller table (TODO: controller for underactuated USV)
-  void create_controller_table(int m) {
-    // real-time data in the controller
-    std::string str =
-        "CREATE TABLE controller"
-        "(ID          INTEGER PRIMARY KEY AUTOINCREMENT,"
-        " DATETIME    TEXT       NOT NULL,"
-        " tauX        DOUBLE, "
-        " tauY        DOUBLE, "
-        " tauMz       DOUBLE, "; /* tau: desired force */
+  void create_controller_table() {
+    try {
+      // real-time data in the controller
+      std::string str =
+          "CREATE TABLE controller"
+          "(ID          INTEGER PRIMARY KEY AUTOINCREMENT,"
+          " DATETIME    TEXT       NOT NULL";
 
-    // the angle of each propeller
-    for (int i = 0; i != m; ++i) {
-      str += " alpha" + std::to_string(i + 1) + " INT,";
-    }
-    // the speed of each propeller
-    for (int i = 0; i != m; ++i) {
-      str += " rpm" + std::to_string(i + 1) + " INT,";
-    }
-    // BalphaU: estimated force
-    str +=
-        " estX        DOUBLE, "
-        " estY        DOUBLE, "
-        " estMz       DOUBLE);";
+      // tau: desired force
+      for (int i = 0; i != n; ++i) {
+        str += " ,tau" + std::to_string(i + 1) + " DOUBLE";
+      }
+      // the angle of each propeller
+      for (int i = 0; i != m; ++i) {
+        str += " ,alpha" + std::to_string(i + 1) + " INT";
+      }
+      // the speed of each propeller
+      for (int i = 0; i != m; ++i) {
+        str += " ,rpm" + std::to_string(i + 1) + " INT";
+      }
+      // BalphaU: estimated force
+      for (int i = 0; i != n; ++i) {
+        str += " ,est" + std::to_string(i + 1) + " DOUBLE";
+      }
+      str += ");";
+      db << str;
 
-    db << str;
+    } catch (sqlite::sqlite_exception e) {
+      CLOG(ERROR, "sql") << e.what();
+    }
   }
 
   // create each estimator table
   void create_estimator_table() {
-    // real-time data in the estimator
-    std::string str =
-        "CREATE TABLE estimator"
-        "(ID          INTEGER PRIMARY KEY AUTOINCREMENT,"
-        " DATETIME    TEXT       NOT NULL,"
-        " meas_x      DOUBLE, "
-        " meas_y      DOUBLE, "
-        " meas_theta  DOUBLE, "
-        " meas_u      DOUBLE, "
-        " meas_v      DOUBLE, "
-        " meas_r      DOUBLE, " /* Measurement */
-        " state_x     DOUBLE, "
-        " state_y     DOUBLE, "
-        " state_theta DOUBLE, "
-        " state_u     DOUBLE, "
-        " state_v     DOUBLE, "
-        " state_r     DOUBLE, " /* state */
-        " perror_x    DOUBLE, "
-        " perror_y    DOUBLE, "
-        " perror_mz   DOUBLE, " /* perror */
-        " verror_x    DOUBLE, "
-        " verror_y    DOUBLE, "
-        " verror_mz   DOUBLE); "; /* verror */
+    try {
+      // real-time data in the estimator
+      std::string str =
+          "CREATE TABLE estimator"
+          "(ID          INTEGER PRIMARY KEY AUTOINCREMENT,"
+          " DATETIME    TEXT       NOT NULL,"
+          " meas_x      DOUBLE, "
+          " meas_y      DOUBLE, "
+          " meas_theta  DOUBLE, "
+          " meas_u      DOUBLE, "
+          " meas_v      DOUBLE, "
+          " meas_r      DOUBLE, " /* Measurement */
+          " state_x     DOUBLE, "
+          " state_y     DOUBLE, "
+          " state_theta DOUBLE, "
+          " state_u     DOUBLE, "
+          " state_v     DOUBLE, "
+          " state_r     DOUBLE, " /* state */
+          " perror_x    DOUBLE, "
+          " perror_y    DOUBLE, "
+          " perror_mz   DOUBLE, " /* perror */
+          " verror_x    DOUBLE, "
+          " verror_y    DOUBLE, "
+          " verror_mz   DOUBLE); "; /* verror */
 
-    db << str;
+      db << str;
+
+    } catch (sqlite::sqlite_exception e) {
+      CLOG(ERROR, "sql") << e.what();
+    }
   }
 
   // create planner table (TODO)
@@ -191,15 +269,59 @@ class database {
     _str += std::to_string(_gpsRTdata.NSV1);
     _str += ", ";
     _str += std::to_string(_gpsRTdata.NSV2);
-    _str += ", ";
+    _str += ", '";
     _str += std::string(1, _gpsRTdata.status);
+    _str += "' , '";
+    _str += std::string(_gpsRTdata.check);
+    _str += "' , ";
+    _str += std::to_string(_gpsRTdata.UTM_x);
     _str += ", ";
-    _str += std::string(check);
-    _str += ", ";
-    _str += std::to_string(UTM_x);
-    _str += ", ";
-    _str += std::to_string(UTM_y);
+    _str += std::to_string(_gpsRTdata.UTM_y);
   }
-  void convert2string(const estimatorRTdata &_RTdata, std::string &_str) {}
+  void convert2string(const controllerRTdata<m, n> &_RTdata,
+                      std::string &_str) {
+    // tau: desired force
+    for (int i = 0; i != n; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.tau(i));
+    }
+    // the angle of each propeller
+    for (int i = 0; i != m; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.alpha_deg(i));
+    }
+    // the speed of each propeller
+    for (int i = 0; i != m; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.rotation(i));
+    }
+    // BalphaU: estimated force
+    for (int i = 0; i != n; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.BalphaU(i));
+    }
+  }
+  void convert2string(const estimatorRTdata &_RTdata, std::string &_str) {
+    // Measurement
+    for (int i = 0; i != 6; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.Measurement(i));
+    }
+    // State
+    for (int i = 0; i != 6; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.State(i));
+    }
+    // Perror
+    for (int i = 0; i != 3; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.p_error(i));
+    }
+    // verror
+    for (int i = 0; i != 3; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.v_error(i));
+    }
+  }
 };
 #endif /* _DATABASE_H_ */
---
+++ fileIO/test/testsqlite.cc
@@ -0,0 +1,74 @@
+/*
+*******************************************************************************
+* testsqlite.cc: unit test for modern sqlite wrapper
+* This header file can be read by C++ compilers
+*
+* by Hu.ZH(CrossOcean.ai)
+*******************************************************************************
+*/
+
+#include "database.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+int main() {
+  const int m = 3;
+  const int n = 3;
+  el::Loggers::addFlag(el::LoggingFlag::CreateLoggerAutomatically);
+
+  LOG(INFO) << "The program has started!";
+
+  database<n, n> _sqlitetest("dbtest.db");
+  _sqlitetest.initializetables();
+  // real time GPS/IMU data
+  gpsRTdata gps_data{
+      0,                // date
+      0,                // time
+      0,                // heading
+      0,                // pitch
+      0,                // roll
+      0,                // latitude
+      0,                // longitude
+      0,                // altitude
+      0,                // Ve
+      0,                // Vn
+      0,                // Vu
+      0,                // base_line
+      0,                // NSV1
+      0,                // NSV2
+      '0',              // status
+      {'a', 'b', '0'},  // check
+      0,                // UTM_x
+      0                 // UTM_y
+  };
+  // realtime parameters of the estimators
+  estimatorRTdata _estimatorRTdata{
+      Eigen::Matrix3d::Identity(),          // CTB2G
+      Eigen::Matrix3d::Identity(),          // CTG2B
+      Eigen::Matrix<double, 6, 1>::Zero(),  // Measurement
+      Eigen::Matrix<double, 6, 1>::Zero(),  // State
+      Eigen::Vector3d::Zero(),              // p_error
+      Eigen::Vector3d::Zero(),              // v_error
+      Eigen::Vector3d::Zero(),              // BalphaU
+      Eigen::Matrix<double, 6, 1>::Zero()   // motiondata_6dof
+  };
+  controllerRTdata<m, n> _controllerRTdata{
+      (Eigen::Matrix<double, n, 1>() << 0, 0, 1).finished(),    // tau
+      Eigen::Matrix<double, n, 1>::Zero(),                      // BalphaU
+      (Eigen::Matrix<double, m, 1>() << 0, 0.5, 0).finished(),  // u
+      // vectormi()::Zero(),                    // rotation
+      (Eigen::Matrix<int, m, 1>() << 100, 500, 400).finished(),
+      (Eigen::Matrix<double, m, 1>() << M_PI / 2, -M_PI / 2, M_PI * 2 / 3)
+          .finished(),                  // alpha
+      Eigen::Matrix<int, m, 1>::Zero()  // alpha_deg
+
+  };
+  for (int i = 0; i != 3; ++i) {
+    _sqlitetest.update_gps_table(gps_data);
+    _sqlitetest.update_estimator_table(_estimatorRTdata);
+    _sqlitetest.update_controller_table(_controllerRTdata);
+  }
+
+  LOG(INFO) << "Shutting down.";
+  return 0;
+}
