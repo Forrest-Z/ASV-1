fe4543b99b7dcbd8a11ad59b9ce89a3f1d20e4f8
YoungCapta1n <huzhuhuan1991@gmail.com>
2019-05-27 10:37:58 +0800

    serial communication

--- GUI_communication/include/guiserver.h
+++ GUI_communication/include/guiserver.h
@@ -14,47 +14,68 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include "controllerdata.h"
+#include "easylogging++.h"
+#include "estimatordata.h"
+#include "gpsdata.h"
+#include "plannerdata.h"
 #include "serial/serial.h"
 #include "timecounter.h"
 
+template <int m, int n = 3>
 class guiserver {
+  template <int _m, int _n>
+  friend std::ostream &operator<<(std::ostream &, const guiserver<_m, _n> &);
+
  public:
   guiserver()
-      : my_serial("/dev/ttyUSB0", 19200, serial::Timeout::simpleTimeout(1000)) {
-    if (my_serial.isOpen())
-      std::cout << " serial initialization successful!" << std::endl;
+      : my_serial("/dev/ttyUSB0", 19200, serial::Timeout::simpleTimeout(500)),
+        send_buffer(""),
+        recv_buffer(""),
+        count(0) {
+    judgeserialstatus();
   }
   ~guiserver() {}
 
-  void testcommunication() {
-    timecounter _timer;
-    long int et = _timer.timeelapsed();
-    my_serial.setTimeout(serial::Timeout::max(), 100, 0, 100, 0);
+  void convertalldata2string(const controllerRTdata<m, n> &_controllerRTdata,
+                             const estimatorRTdata &_estimatorRTdata,
+                             const plannerRTdata &_plannerRTdata,
+                             const gpsRTdata &_gpsRTdata) {
+    send_buffer.clear();
+    recv_buffer.clear();
+    send_buffer = "$TEST" + std::to_string(++count);
+    convert2string(_controllerRTdata, send_buffer);
+    convert2string(_estimatorRTdata, send_buffer);
+    convert2string(_plannerRTdata, send_buffer);
+    convert2string(_gpsRTdata, send_buffer);
+    send_buffer += "\n";
 
-    std::string recv_buffer = "";
-    int i = 0;
-    while (1) {
-      ++i;
-      std::cout << "Iteration " << i << std::endl;
+    size_t bytes_wrote = my_serial.write(send_buffer);
+    my_serial.flush();
+    if (my_serial.waitReadable()) recv_buffer = my_serial.readline(20);
+    my_serial.flush();
+  }
 
-      std::string send_buffer = "send " + std::to_string(i + 1) + "\n";
-      // star to time
-      size_t bytes_wrote = my_serial.write(send_buffer);
-      if (my_serial.waitReadable()) recv_buffer = my_serial.readline(20);
-      my_serial.flush();
-      et = _timer.timeelapsed();
-      std::cout << "The string sent is: " << send_buffer
-                << " bytes_wrote: " << bytes_wrote << std::endl;
-      std::cout << "The string recv is: " << recv_buffer << std::endl;
-      std::cout << "elapsed time:" << et << std::endl;
+  void convertalldata2string(const controllerRTdata<m, n> &_controllerRTdata,
+                             const estimatorRTdata &_estimatorRTdata,
+                             const plannerRTdata &_plannerRTdata) {
+    send_buffer.clear();
+    recv_buffer.clear();
+    send_buffer = "$TEST" + std::to_string(++count);
+    convert2string(_controllerRTdata, send_buffer);
+    convert2string(_estimatorRTdata, send_buffer);
+    convert2string(_plannerRTdata, send_buffer);
+    send_buffer += "\n";
 
-      // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+    size_t bytes_wrote = my_serial.write(send_buffer);
+    if (my_serial.waitReadable()) recv_buffer = my_serial.readline(20);
   }
 
  private:
   serial::Serial my_serial;
-
+  std::string send_buffer;
+  std::string recv_buffer;
+  int count;
   void enumerate_ports() {
     std::vector<serial::PortInfo> devices_found = serial::list_ports();
 
@@ -66,6 +87,98 @@ class guiserver {
                 << ", " << device.hardware_id.c_str() << std::endl;
     }
   }
+
+  void judgeserialstatus() {
+    if (my_serial.isOpen())
+      CLOG(INFO, "gui-serial") << " serial port open successful!";
+    else
+      CLOG(INFO, "gui-serial") << " serial port open failure!";
+  }
+
+  // convert real time GPS data to sql string
+  void convert2string(const gpsRTdata &_gpsRTdata, std::string &_str) {
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.date);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.time);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.heading);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.pitch);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.roll);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.latitude);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.longitude);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.altitude);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.Ve);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.Vn);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.Vu);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.base_line);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.NSV1);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.NSV2);
+    _str += ", '";
+    _str += std::string(1, _gpsRTdata.status);
+    _str += "' , '";
+    _str += std::string(_gpsRTdata.check);
+    _str += "' , ";
+    _str += std::to_string(_gpsRTdata.UTM_x);
+    _str += ", ";
+    _str += std::to_string(_gpsRTdata.UTM_y);
+  }
+  void convert2string(const controllerRTdata<m, n> &_RTdata,
+                      std::string &_str) {
+    // the angle of each propeller
+    for (int i = 0; i != m; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.alpha_deg(i));
+    }
+    // the speed of each propeller
+    for (int i = 0; i != m; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.rotation(i));
+    }
+  }
+  void convert2string(const estimatorRTdata &_RTdata, std::string &_str) {
+    // Measurement
+    for (int i = 0; i != 6; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.Measurement(i));
+    }
+    // State
+    for (int i = 0; i != 6; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.State(i));
+    }
+  }
+
+  void convert2string(const plannerRTdata &_RTdata, std::string &_str) {
+    // setpoint
+    for (int i = 0; i != 3; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.setpoint(i));
+    }
+    // v_setpoint
+    for (int i = 0; i != 3; ++i) {
+      _str += ", ";
+      _str += std::to_string(_RTdata.v_setpoint(i));
+    }
+  }
 };
+
+template <int _m, int _n>
+std::ostream &operator<<(std::ostream &os,
+                         const guiserver<_m, _n> &_guiserver) {
+  os << "Buffer sent is: " << _guiserver.send_buffer;
+  os << "Buffer recv is: " << _guiserver.recv_buffer << std::endl;
+}
 
 #endif /* _GUISERVER_H_ */
--- GUI_communication/test/testguicommunication.cc
+++ GUI_communication/test/testguicommunication.cc
@@ -10,7 +10,48 @@
 
 #include "guiserver.h"
 
+INITIALIZE_EASYLOGGINGPP
+
 int main() {
-  guiserver _guiserver;
-  _guiserver.testcommunication();
+  constexpr int num_thruster = 1;
+  constexpr int dim_controlspace = 3;
+
+  guiserver<num_thruster, dim_controlspace> _guiserver;
+
+  plannerRTdata _plannerRTdata{
+      Eigen::Vector3d::Zero(),  // setpoint
+      Eigen::Vector3d::Zero(),  // v_setpoint
+      Eigen::Vector2d::Zero(),  // waypoint0
+      Eigen::Vector2d::Zero(),  // waypoint1
+      Eigen::Vector3d::Zero()   // command
+  };
+
+  controllerRTdata<num_thruster, dim_controlspace> _controllerRTdata{
+      Eigen::Matrix<double, dim_controlspace, 1>::Zero(),  // tau
+      Eigen::Matrix<double, dim_controlspace, 1>::Zero(),  // BalphaU
+      Eigen::Matrix<double, num_thruster, 1>::Zero(),      // u
+      Eigen::Matrix<int, num_thruster, 1>::Zero(),         // rotation
+      Eigen::Matrix<double, num_thruster, 1>::Zero(),      // alpha
+      Eigen::Matrix<int, num_thruster, 1>::Zero()          // alpha_deg
+  };
+
+  // realtime parameters of the estimators
+  estimatorRTdata _estimatorRTdata{
+      Eigen::Matrix3d::Identity(),          // CTB2G
+      Eigen::Matrix3d::Identity(),          // CTG2B
+      Eigen::Matrix<double, 6, 1>::Zero(),  // Measurement
+      Eigen::Matrix<double, 6, 1>::Zero(),  // State
+      Eigen::Vector3d::Zero(),              // p_error
+      Eigen::Vector3d::Zero(),              // v_error
+      Eigen::Vector3d::Zero(),              // BalphaU
+      Eigen::Matrix<double, 6, 1>::Zero()   // motiondata_6dof
+  };
+
+  timecounter _timer;
+  while (1) {
+    _guiserver.convertalldata2string(_controllerRTdata, _estimatorRTdata,
+                                     _plannerRTdata);
+    std::cout << _timer.timeelapsed() << std::endl;
+    std::cout << _guiserver;
+  }
 }
